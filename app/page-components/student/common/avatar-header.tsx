fbaef1d9b8ebe642644646fd967c363e44549168
Hieu Dong <hieudh@dgroup.co>
2022-08-23 22:30:17 +0700

    header gradient for avatar
    Signed-off-by: Hieu Dong <hieudh@dgroup.co>

--- app/page-components/student/common/avatar-header.tsx
+++ app/page-components/student/common/avatar-header.tsx
@@ -1,9 +1,27 @@
+import classNames from "classnames";
+import { useMemo } from "react";
+import { getGradient } from "~/libs/gradients";
+
 export interface AvatarHeaderProps {
   url?: string;
   name: string;
 }
 
 export const AvatarHeader = (props: AvatarHeaderProps) => {
+  const name = useMemo(() => {
+    if (!props.name) return "U";
+    const names = props.name.split(/\s+/);
+    if (names.length < 2) return names[0][0].toUpperCase();
+
+    return [names.shift() || "", names.pop() || ""]
+      .map((n) => n[0].toUpperCase())
+      .join("");
+  }, [props.name]);
+
+  const colorClass = useMemo(() => {
+    return getGradient(props.name);
+  }, [props.name]);
+
   return props.url ? (
     <img
       className="inline-block h-[2.375rem] w-[2.375rem] rounded-full ring-2 ring-white dark:ring-gray-800"
@@ -11,8 +29,15 @@ export const AvatarHeader = (props: AvatarHeaderProps) => {
       alt="Avatar of user"
     />
   ) : (
-    <div className="relative inline-flex h-10 w-10 items-center justify-center overflow-hidden rounded-full bg-gray-100 dark:bg-gray-600">
-      <span className="font-medium text-gray-600 dark:text-gray-300">JL</span>
+    <div
+      className={classNames(
+        "relative inline-flex h-10 w-10 items-center justify-center overflow-hidden rounded-full",
+        colorClass
+      )}
+    >
+      <span className="font-medium text-gray-600 dark:text-gray-300">
+        {name}
+      </span>
     </div>
   );
 };
